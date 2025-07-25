24d7f800053bf4185cedb0e0ce31093bcdcbd0aa
Demetrius Edwards <edwardsd03@outlook.com>
2025-07-25 07:45:47 -0500

    Deployment fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 function App() {
